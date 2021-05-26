a5215d78c903b2a597ccbbc09cd9edbbf76f83d7
dmartinchick <dmartinchick@gmail.com>
2021-05-26 18:55:28 +0300

    add setings
    Проведена базовая настройка

--- data/config.py
+++ data/config.py
@@ -4,6 +4,13 @@ from environs import Env
 env = Env()
 env.read_env()
 
+# Данные для подключения к Телеграммботу
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+# Данные для подключения к базе данных
+HOST = env.str("HOST")
+PORT = env.int("PORT")
+USER = env.str("USER")
+PASSWORD = env.str("PASSWORD")
